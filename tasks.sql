9914d62919f84ea918794781dc8e2aca3653769f
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-05-14 18:19:44 +0300

    Update tasks.sql

--- tasks.sql
+++ tasks.sql
@@ -25,7 +25,7 @@ FROM employees e
 JOIN employees s ON e.employee_id = s.manager_id
 GROUP BY e.employee_id, e.first_name, e.last_name;
 ----------------------------------------------------------------------------------
---тут я считаю количество лет с момента наёма 
+--тут я считаю количество лет с момента найма 
 
 select employees.first_name, employees.last_name,
 EXTRACT(YEAR from sysdate) - EXTRACT(year from hire_date) from employees where extract(month from hire_date) = EXTRACT(month from sysdate)
